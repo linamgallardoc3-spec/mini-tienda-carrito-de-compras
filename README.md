4c8d1b1d475fc7adabaef93c7d52ff36bb9bdf94
Lina María Gallardo Corrales <linamgallardoc3@gmail.com>
2026-06-13 11:53:12 -0500

    Detalles finales

---
+++ README.md
@@ -0,0 +1,6 @@
+## Mini Tienda con Carrito de Compras
+
+# Manipulación del DOM · Eventos · Bootstrap 5
+
+En este laboratorio construirás desde cero una mini tienda en línea con carrito de compras funcional. Usarás HTML para la estructura, Bootstrap para el diseño visual y JavaScript para hacer que todo cobre vida al interactuar con el DOM.
+Al finalizar tendrás una página donde el usuario puede agregar productos al carrito, eliminarlos individualmente ver el total actualizado y vaciar el carrito — todo sin recargar la página.
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const botonesAgregar = document.querySelectorAll('.btn-agregar');
+const botonVaciarCarrito = document.getElementById('btn-vaciar');
+const idMensaje = document.getElementById('mensaje');
 let cantidadItems = 0;
 let totalVenta = 0;
 
@@ -9,13 +11,25 @@ botonesAgregar.forEach(botonAgrerar => {
         agregarAlCarrito(nombreProducto,precioProducto);
     });
 });
+botonVaciarCarrito.addEventListener('click', function () {
+    const listaCarrito = document.querySelectorAll('.lista-producto');
+    listaCarrito.forEach(producto => {
+        producto.remove();
+    });
+    idMensaje.style.display = 'block'
+    totalVenta = 0;
+    cantidadItems = 0;
+    updateBadge();
+    updateTotal();
+})
 
 function agregarAlCarrito(nombre,precio) {
     let precioIn = parseInt(precio);
     totalVenta = totalVenta + precioIn;
+    idMensaje.style.display = 'none'
     const listaCarrito = document.getElementById('lista-carrito');
     let lista = document.createElement('li');
-    lista.className = 'list-group-item d-flex justify-content-between align-items-center';
+    lista.className = 'lista-producto list-group-item d-flex justify-content-between align-items-center';
         lista.innerHTML = `
             <strong>${nombre}</strong><small class="text-muted">$${precioIn}</small><button class="btn-eliminar btn btn-danger"><i class="bi bi-x-square"></i></button>
         `;
